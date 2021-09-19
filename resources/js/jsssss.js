a17c457479aa687b34cca0d3118ddd963bcb9657
Sergey <s-vesel94@ya.ru>
2021-09-19 16:46:04 +0300

    -

--- resources/js/jsssss.js
+++ resources/js/jsssss.js
@@ -1,1 +1,8 @@
 /* JS */
+document.body.querySelector('.menu').addEventListener('click', () => {
+    document.body.querySelector('.left-menu').classList.toggle('hide');
+});
+
+document.body.querySelector('.shadow-menu').addEventListener('click', () => {
+    document.body.querySelector('.left-menu').classList.toggle('hide');
+});
