a66d4fed12ca6c41a7b86c49b0bcc44fff7091c7
Sergey <veselok@gmail.com>
2021-10-16 16:49:13 +0300

    +

--- resources/views/basket/index.blade.php
+++ resources/views/basket/index.blade.php
@@ -36,42 +36,44 @@
                                          alt="Изображение {{$product->title}}">
                                 @endforeach
 
-                                <div class="flex-a ml-10">
-                                    <a class="product-name-in-basket cp font-semibold"
-                                       href="{{route('product', [$product->Product->Category->semantic_url, $product->Product->semantic_url])}}">
-                                        <div>{{$product->title}}</div>
-                                    </a>
-                                    <div>{{$product->count . ' ' . $product->price}}</div>
-                                </div>
-
-                                <div class="flex">
-                                    <button class="button-delete-product-in-basket cp clear-button"
-                                            data-product-id="{{$product->id}}"
-                                            data-product-price-id="{{$product->price_id}}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40"
-                                             fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
-                                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
-                                        </svg>
-                                    </button>
-                                    <div>
-                                        <input data-product-id="{{$product->id}}"
-                                               data-product-price-id="{{$product->price_id}}"
-                                               class="input-count-product-in-basket clear-button"
-                                               data-count-product="{{$product->id . '-' . $product->price_id}}"
-                                               value="{{$productsInBasket[$product->id][$product->price_id]['count']}}"
-                                               type="text" autocomplete="off" maxlength="2"
-                                               style="font-size: 20px; cursor: default; border: unset; width: 40px; height: 40px; text-align: center;">
+                                <div class="flex-wrap flex-a">
+                                    <div class="flex-a ml-10">
+                                        <a class="product-name-in-basket cp font-semibold"
+                                           href="{{route('product', [$product->Product->Category->semantic_url, $product->Product->semantic_url])}}">
+                                            <div>{{$product->title}}</div>
+                                        </a>
+                                        <div>{{$product->count . ' ' . $product->price}}</div>
                                     </div>
-                                    <button class="button-add-product-in-basket cp clear-button"
-                                            data-product-id="{{$product->id}}"
-                                            data-product-price-id="{{$product->price_id}}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40"
-                                             fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                                        </svg>
-                                    </button>
+
+                                    <div class="flex">
+                                        <button class="button-delete-product-in-basket cp clear-button"
+                                                data-product-id="{{$product->id}}"
+                                                data-product-price-id="{{$product->price_id}}">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40"
+                                                 fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
+                                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+                                            </svg>
+                                        </button>
+                                        <div>
+                                            <input data-product-id="{{$product->id}}"
+                                                   data-product-price-id="{{$product->price_id}}"
+                                                   class="input-count-product-in-basket clear-button"
+                                                   data-count-product="{{$product->id . '-' . $product->price_id}}"
+                                                   value="{{$productsInBasket[$product->id][$product->price_id]['count']}}"
+                                                   type="text" autocomplete="off" maxlength="2"
+                                                   style="font-size: 20px; cursor: default; border: unset; width: 40px; height: 40px; text-align: center;">
+                                        </div>
+                                        <button class="button-add-product-in-basket cp clear-button"
+                                                data-product-id="{{$product->id}}"
+                                                data-product-price-id="{{$product->price_id}}">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40"
+                                                 fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                                            </svg>
+                                        </button>
+                                    </div>
                                 </div>
 
                             </div>
