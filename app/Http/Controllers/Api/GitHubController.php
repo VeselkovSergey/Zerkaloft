4a4db4b54a98f4c3dd81b8fe71a940dde7f79cac
Sergey <veselok@gmail.com>
2021-10-16 23:47:02 +0300

    +

--- app/Http/Controllers/Api/GitHubController.php
+++ app/Http/Controllers/Api/GitHubController.php
@@ -9,9 +9,9 @@ class GitHubController extends ApiController
 {
     public function Push(Request $request)
     {
-        echo 'git pull start' . PHP_EOL;
         // sudo -u www-data ssh-keygen - генерим ssh ключи под www-data
         // chmod 600 /var/www/.ssh/ida_rsa - для работы git pull под www-data
+        echo 'git pull start' . PHP_EOL;
         echo shell_exec('git pull');
         echo 'git pull complete' . PHP_EOL;
     }
