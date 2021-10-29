be6fd501f214d0e05cc9688d64c8e7c1265d023b
Sergey <veselok@gmail.com>
2021-10-29 18:29:46 +0300

    +

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -40,8 +40,8 @@
         <img class="mx-5" width="32" src="{{url('icon/telegram.svg')}}" alt="">
         <img class="mx-5" width="32" src="{{url('icon/viber.svg')}}" alt="">
         <img class="mx-5" width="32" src="{{url('icon/whatsapp.svg')}}" alt="">
-        <img class="mx-5" width="28" src="{{url('icon/mail.svg')}}" alt="">
-        <img class="mx-5" width="28" src="{{url('icon/phone.svg')}}" alt="">
+        <img class="mx-5" width="26" src="{{url('icon/mail.svg')}}" alt="">
+        <img class="mx-5" width="24" src="{{url('icon/phone.svg')}}" alt="">
     </div>
     <div class="phone-container-header flex-center font-semibold pt-25" style="padding-top: 40px;">
         <a class="text-center" style="text-decoration: none;" href="tel:{{env('PHONE_COMPANY')}}">{{env('PHONE_COMPANY')}}</a>
