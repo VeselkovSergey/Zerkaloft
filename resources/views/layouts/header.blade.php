d239704996a07c4ce3771da6b4f0cde96675a6e4
Sergey <veselok@gmail.com>
2021-10-29 18:15:33 +0300

    +

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -41,7 +41,7 @@
         <img class="mx-5" width="32" src="{{url('icon/viber.svg')}}" alt="">
         <img class="mx-5" width="32" src="{{url('icon/whatsapp.svg')}}" alt="">
     </div>
-    <div class="phone-container-header flex-center font-semibold pt-25">
+    <div class="phone-container-header flex-center font-semibold pt-25" style="padding-top: 40px;">
         <a class="text-center" style="text-decoration: none;" href="tel:{{env('PHONE_COMPANY')}}">{{env('PHONE_COMPANY')}}</a>
     </div>
 </div>
