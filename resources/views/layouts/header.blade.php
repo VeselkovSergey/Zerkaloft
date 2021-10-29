53ae1f94efb263c527fa6e38e38a9405672e9e79
Sergey <veselok@gmail.com>
2021-10-29 16:23:28 +0300

    +

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,14 +13,14 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
         </svg>
-        <div class="ml-10 text-menu">КАТАЛОГ</div>
+        <div class="ml-10 text-menu color-pink font-semibold">Каталог</div>
     </div>
 </div>
 
 <div class="search-container-header p-5 flex-center" style="flex: 1;">
     <div class="pos-rel w-100">
         <div class="pos-abs" style="top: 11px; left: 10px; color: grey;">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search color-pink" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
             </svg>
         </div>
