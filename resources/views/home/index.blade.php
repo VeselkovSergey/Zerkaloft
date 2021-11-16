801abca0b640f1aae4b836b991ebdf66f9c4edbd
Sergey <veselok@gmail.com>
2021-11-16 22:25:36 +0300

    -

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -88,16 +88,9 @@
                 @endforeach
 
             @endforeach
-{{--            @for($i = 0; $i < 10; $i++)--}}
-{{--                <div class="smooth-block" style="width: 48%; margin: 1%; /*border: 1px solid black;*/ border-radius: 15px; box-shadow: 0 0 10px rgb(0 0 0 / 75%);">--}}
-{{--                    <img style="width: 100%; height: 350px; border-radius: 15px;" src="img.jpg" alt="">--}}
-{{--                </div>--}}
-{{--            @endfor--}}
         </div>
 
     </div>
-
-
 
 @stop
 
