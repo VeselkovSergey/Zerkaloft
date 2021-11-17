71106b9bea139a7c99be11f59d7994df3f2f7e95
Sergey <veselok@gmail.com>
2021-11-18 00:02:38 +0300

    php artisan migrate

--- app/Http/Controllers/Api/GitHubController.php
+++ app/Http/Controllers/Api/GitHubController.php
@@ -14,5 +14,7 @@ class GitHubController extends ApiController
         echo 'git pull start' . PHP_EOL;
         echo shell_exec('git pull');
         echo 'git pull complete' . PHP_EOL;
+
+        echo shell_exec('php artisan migrate');
     }
 }
