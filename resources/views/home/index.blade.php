e8db6073e46e078f131b6bb8603dc3bcc02c1000
Sergey <veselok@gmail.com>
2021-11-20 13:03:21 +0300

    адаптивные кнопки на слайдере

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -10,16 +10,14 @@
         <div class="carousel mx-a mb-25">
 
             <div class="carousel-container shadow border-radius-15">
-                @php($i = 0)
                 @foreach($carouselImages as $carouselImage)
-                    <div class="img-carousel {{$i === 0 ? 'active' : ''}}">
+                    <div class="img-carousel {{$loop->first ? 'active' : ''}}">
                         <img src="{{route('files', $carouselImage)}}" class="w-100 h-100" alt="">
                     </div>
-                    @php($i++)
                 @endforeach
             </div>
 
-            <div style="position: absolute; top: calc(50% - 24px); left: 24px;">
+            <div class="container-btn-slider" style="position: absolute; top: calc(50% - 24px); left: 24px;">
                 <div class="btn-slider btn-slider-prev">
                     <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
                         <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
@@ -27,7 +25,7 @@
                 </div>
             </div>
 
-            <div style="position: absolute; top: calc(50% - 24px); right: 24px;">
+            <div class="container-btn-slider" style="position: absolute; top: calc(50% - 24px); right: 24px;">
                 <div class="btn-slider btn-slider-next">
                     <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
                         <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
