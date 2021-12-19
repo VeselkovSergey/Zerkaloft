7f7a8eb76f636a8de148686cf3ef9fbe658f0384
Sergey <veselok@gmail.com>
2021-12-19 13:00:08 +0300

    поиск

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,7 @@
     </a>
 </div>
 
-<div class="flex-center search-button-small-screen-container" style="width: 40%">
+<div class="flex-center search-button-small-screen-container" style="width: 45%">
     <div  class="border-radius-5" style="border: 1px solid #2e3192; width: 100%; padding: 3px 0 3px 3px">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search color-pink" viewBox="0 0 16 16">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
