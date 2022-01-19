06cb984e44968fab05f46b563c2d9e77d50942ad
Sergey <veselok@gmail.com>
2022-01-19 22:18:26 +0300

    сортировка для поиска

--- app/Http/Controllers/Calculator/CalculatorController.php
+++ app/Http/Controllers/Calculator/CalculatorController.php
@@ -57,6 +57,7 @@ class CalculatorController
     {
         $categoryId = $request->categoryId;
         $modification = $request->modification;
+        sort($modification);
         $modificationProcessed = implode('-', $modification);
         $product = Products::where('category_id', $categoryId)->where('modification_id', $modificationProcessed)->first();
         if (empty($product)) {
