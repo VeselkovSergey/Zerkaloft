401e83241267905d0e85dd7100d347e631260ff0
Sergey <veselok@gmail.com>
2022-02-25 10:38:35 +0300

    meta yandex

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -28,6 +28,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="yandex-verification" content="1bc095e408e6a616" />
         @yield('meta')
 
         <title>{{ isset($title_page) ? $title_page : env('APP_NAME') }}</title>
