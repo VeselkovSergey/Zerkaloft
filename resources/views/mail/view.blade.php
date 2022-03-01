17c03a105c68339e55c9a554476d1709fa78d8e6
Sergey <veselok@gmail.com>
2022-03-01 18:43:42 +0300

    почтовик

---
+++ resources/views/mail/view.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <div style="width: 300px;">
+        <img src="{{asset('icon/logo_rus.svg')}}" alt="">
+    </div>
+    <h3>Добро пожаловать Victoria Рекламное агенство</h3>
+    <div>Вы успешно зарегистрировались на сайте vivepro.ru</div>
+    <div>Для входа в личный кабинет используйте:</div>
+    <div>Логин: </div>
+    <div>Пароль: </div>
+    <div>С уважением,</div>
+    <div>Victoria Рекламное агенство</div>
+</div>
