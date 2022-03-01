0a951c0aacbaee2b7ac06d9a4f8b2b211290fe52
Sergey <veselok@gmail.com>
2022-03-01 18:52:01 +0300

    почтовик

--- resources/views/mail/view.blade.php
+++ resources/views/mail/view.blade.php
@@ -1,12 +1,17 @@
-<div>
+<div style="display: flex; flex-direction: column; align-items: center">
     <div style="width: 300px;">
         <img width="200" height="60" src="{{asset('icon/logo_rus.png')}}" alt="">
     </div>
-    <h3>Добро пожаловать Victoria Рекламное агенство</h3>
-    <div>Вы успешно зарегистрировались на сайте vivepro.ru</div>
-    <div>Для входа в личный кабинет используйте:</div>
-    <div>Логин: </div>
-    <div>Пароль: </div>
-    <div>С уважением,</div>
-    <div>Victoria Рекламное агенство</div>
+    <div style="display: flex; flex-direction: column; align-items: center">
+        <h3>Добро пожаловать Victoria Рекламное агенство</h3>
+        <div>Вы успешно зарегистрировались на сайте vivepro.ru</div>
+        <div>Для входа в личный кабинет используйте:</div>
+        <div>Логин: </div>
+        <div>Пароль: </div>
+        <div style="text-align: left; width: 100%">
+            <div>С уважением,</div>
+            <div>Victoria Рекламное агенство</div>
+        </div>
+    </div>
+
 </div>
