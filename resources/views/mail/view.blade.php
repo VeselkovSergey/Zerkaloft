87ac9ce442f3b2cf16c090aea5a6721763c4787b
Sergey <veselok@gmail.com>
2022-03-01 18:58:25 +0300

    почтовик

--- resources/views/mail/view.blade.php
+++ resources/views/mail/view.blade.php
@@ -1,4 +1,4 @@
-<div style="text-align: center;">
+<div style="text-align: center; background-image: url('{{asset('img/bg.jpeg')}}'); background-repeat: no-repeat; background-size: contain; background-attachment: fixed;">
     <div>
         <img width="200" height="60" src="{{asset('icon/logo_rus.png')}}" alt="">
     </div>
@@ -6,8 +6,8 @@
         <h3>Добро пожаловать Victoria Рекламное агенство</h3>
         <div>Вы успешно зарегистрировались на сайте vivepro.ru</div>
         <div>Для входа в личный кабинет используйте:</div>
-        <div>Логин: </div>
-        <div>Пароль: </div>
+        <div>Логин:</div>
+        <div>Пароль:</div>
         <div style="text-align: left; width: 100%">
             <div>С уважением,</div>
             <div>Victoria Рекламное агенство</div>
