554b486a3d6d7e768b604b409a54e6cf6c0ba464
Sergey <veselok@gmail.com>
2022-03-01 23:50:13 +0300

    почтовик

--- resources/views/mail/view.blade.php
+++ resources/views/mail/view.blade.php
@@ -4,13 +4,13 @@
     </div>
     <div style="text-align: center;">
         <h3>Добро пожаловать</h3>
-        <h3>Victoria Рекламное агентство</h3>
+        <h3 style="color: #2e3192;">Victoria Рекламное агентство</h3>
         <div>Для входа в личный кабинет используйте:</div>
-        <div>Логин: {{$email}}</div>
-        <div>Пароль: {{$password}}</div>
+        <div><span style="color: #2e3192;">Логин:</span> {{$email}}</div>
+        <div><span style="color: #2e3192;">Пароль:</span> {{$password}}</div>
         <div style="text-align: left; width: 100%">
             <div>С уважением,</div>
-            <div>Victoria Рекламное агентство</div>
+            <div style="color: #2e3192;">Victoria Рекламное агентство</div>
         </div>
     </div>
 </div>
