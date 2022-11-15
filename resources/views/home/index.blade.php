2ed8efe1018cea3e2472cc6ebe1a5b4054f737cc
Sergey <veselok@gmail.com>
2022-11-15 23:39:24 +0300

    search page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,48 +4,53 @@
 
     <div>
 
-        @if(isset($carouselImages))
-{{--Для онлайн заказа (та же страница но без слайдера)--}}
+        @if(!isset($searchQuery))
 
-        @if(count($carouselImages))
+            @if(isset($carouselImages))
+                {{--Для онлайн заказа (та же страница но без слайдера)--}}
 
-                <div class="carousel mx-a mb-25">
+                @if(count($carouselImages))
 
-                    <div class="carousel-container shadow border-radius-15 flex">
-                        @foreach($carouselImages as $carouselImage)
-                            <a href="{{$carouselImage->link}}" class="img-carousel">
-                                <img src="{{route('files', $carouselImage->fileId)}}" class="w-100 h-100" alt="">
-                            </a>
-                        @endforeach
-                    </div>
+                    <div class="carousel mx-a mb-25">
 
-                    <div class="container-btn-slider" style="position: absolute; top: calc(50% - 24px); left: 24px;">
-                        <div class="btn-slider btn-slider-prev">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
-                                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
-                            </svg>
+                        <div class="carousel-container shadow border-radius-15 flex">
+                            @foreach($carouselImages as $carouselImage)
+                                <a href="{{$carouselImage->link}}" class="img-carousel">
+                                    <img src="{{route('files', $carouselImage->fileId)}}" class="w-100 h-100" alt="">
+                                </a>
+                            @endforeach
                         </div>
-                    </div>
 
-                    <div class="container-btn-slider" style="position: absolute; top: calc(50% - 24px); right: 24px;">
-                        <div class="btn-slider btn-slider-next">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-                            </svg>
+                        <div class="container-btn-slider" style="position: absolute; top: calc(50% - 24px); left: 24px;">
+                            <div class="btn-slider btn-slider-prev">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+                                    <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+                                </svg>
+                            </div>
                         </div>
+
+                        <div class="container-btn-slider" style="position: absolute; top: calc(50% - 24px); right: 24px;">
+                            <div class="btn-slider btn-slider-next">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                                    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+                                </svg>
+                            </div>
+                        </div>
+
                     </div>
 
+                @endif
+
+            @else
+
+                <div class="font-semibold text-center">
+                    {{\App\Http\Controllers\Administration\SettingsController::OnlineOrderInfo()->text}}
                 </div>
 
-        @endif
-
-        @else
-
-        <div class="font-semibold text-center">
-            {{\App\Http\Controllers\Administration\SettingsController::OnlineOrderInfo()->text}}
-        </div>
+            @endif
 
         @endif
+
 
         <div class="flex-wrap-center w-100">
 
