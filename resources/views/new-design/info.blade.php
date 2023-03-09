f740ebf2507216993f78fe9492f7e5882e12af83
Sergey <veselok@gmail.com>
2023-03-09 14:53:27 +0300

    new design текст слева на info

--- resources/views/new-design/info.blade.php
+++ resources/views/new-design/info.blade.php
@@ -1,67 +1,67 @@
 <div class="flex-center-x p-20 bg-pink" style="color: black;">
-    <div class="text-center scale-1 w-15 flex-column-center-x">
+    <div class="scale-1 w-15 flex-column-center-x">
         <div>
             <img height="120" src="/assets/imgs/info.svg" alt="">
         </div>
         <div>
             ИНФОРМАЦИЯ
         </div>
-        <div>
+        <div style="padding: 0 25px;">
             Небольшой рассказ о маркетинге статьи из журналов изменения в закондательстве
         </div>
     </div>
-    <div class="text-center scale-1 w-15 flex-column-center-x">
+    <div class="scale-1 w-15 flex-column-center-x">
         <div>
             <img height="120" src="/assets/imgs/online-order.svg" alt="">
         </div>
-        <div>
+        <div class="text-center">
             ОНЛАЙН ЗАКАЗ
         </div>
-        <div>
+        <div style="padding: 0 25px;">
             предлагаем вашему вниманию подобранные параметры для быстрого заказа
         </div>
     </div>
-    <div class="text-center scale-1 w-15 flex-column-center-x">
+    <div class="scale-1 w-15 flex-column-center-x">
         <div>
             <img height="120" src="/assets/imgs/payments.svg" alt="">
         </div>
-        <div>
+        <div class="text-center">
             СПОСОБ ОПЛАТЫ
         </div>
-        <div>
+        <div style="padding: 0 25px;">
             оплачивайте заказ на сайте как физическое лицо или как юридическое
         </div>
     </div>
-    <div class="text-center scale-1 w-15 flex-column-center-x">
+    <div class="scale-1 w-15 flex-column-center-x">
         <div>
             <img height="120" src="/assets/imgs/delivered.svg" alt="">
         </div>
-        <div>
+        <div class="text-center">
             ДОСТАВКА
         </div>
-        <div>
+        <div style="padding: 0 25px;">
             оформите доставку у нас на сайте воспользуйтесь услугами наших партнеров
         </div>
     </div>
-    <div class="text-center scale-1 w-15 flex-column-center-x">
+    <div class="scale-1 w-15 flex-column-center-x">
         <div>
             <img height="120" src="/assets/imgs/forum.svg" alt="">
         </div>
-        <div>
+        <div class="text-center">
             ФОРУМ
         </div>
-        <div>
+        <div style="padding: 0 25px;">
             Расскажите свои истории делитесь своим опытом задавайте вопросы
         </div>
     </div>
-    <div class="text-center scale-1 w-15 flex-column-center-x">
+    <div class="scale-1 w-15 flex-column-center-x">
         <div>
             <img height="120" src="/assets/imgs/gallery.svg" alt="">
         </div>
-        <div>
+        <div class="text-center">
             ГАЛЕРЕЯ
         </div>
-        <div>
+        <div style="padding: 0 25px;">
             Работы выполненные нашей командой не тратье время на поиски подберите себе готовые идеи
         </div>
     </div>
