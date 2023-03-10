dd0c7cc3907d46c8da052f6df5622fd7e9285283
Sergey <veselok@gmail.com>
2023-03-10 16:24:28 +0300

    styles

--- resources/views/new-design/info.blade.php
+++ resources/views/new-design/info.blade.php
@@ -4,65 +4,77 @@
             <img height="120" src="/assets/imgs/info.svg" alt="">
         </div>
         <div>
-            ИНФОРМАЦИЯ
-        </div>
-        <div style="padding: 0 25px;">
-            Небольшой рассказ о маркетинге статьи из журналов изменения в закондательстве
+            <div class="mb-5" style="padding: 0 40px;">
+                ИНФОРМАЦИЯ
+            </div>
+            <div style="padding: 0 40px;">
+                Небольшой рассказ о маркетинге статьи из журналов изменения в закондательстве
+            </div>
         </div>
     </div>
     <div class="scale-1 w-15 flex-column-center-x">
         <div>
             <img height="120" src="/assets/imgs/online-order.svg" alt="">
         </div>
-        <div class="text-center">
-            ОНЛАЙН ЗАКАЗ
-        </div>
-        <div style="padding: 0 25px;">
-            предлагаем вашему вниманию подобранные параметры для быстрого заказа
+        <div>
+            <div class="mb-5" style="padding: 0 40px;">
+                ОНЛАЙН ЗАКАЗ
+            </div>
+            <div style="padding: 0 40px;">
+                предлагаем вашему вниманию подобранные параметры для быстрого заказа
+            </div>
         </div>
     </div>
     <div class="scale-1 w-15 flex-column-center-x">
         <div>
             <img height="120" src="/assets/imgs/payments.svg" alt="">
         </div>
-        <div class="text-center">
-            СПОСОБ ОПЛАТЫ
-        </div>
-        <div style="padding: 0 25px;">
-            оплачивайте заказ на сайте как физическое лицо или как юридическое
+        <div>
+            <div class="mb-5" style="padding: 0 40px;">
+                СПОСОБ ОПЛАТЫ
+            </div>
+            <div style="padding: 0 40px;">
+                оплачивайте заказ на сайте как физическое лицо или как юридическое
+            </div>
         </div>
     </div>
     <div class="scale-1 w-15 flex-column-center-x">
         <div>
             <img height="120" src="/assets/imgs/delivered.svg" alt="">
         </div>
-        <div class="text-center">
-            ДОСТАВКА
-        </div>
-        <div style="padding: 0 25px;">
-            оформите доставку у нас на сайте воспользуйтесь услугами наших партнеров
+        <div>
+            <div class="mb-5" style="padding: 0 40px;">
+                ДОСТАВКА
+            </div>
+            <div style="padding: 0 40px;">
+                оформите доставку у нас на сайте воспользуйтесь услугами наших партнеров
+            </div>
         </div>
     </div>
     <div class="scale-1 w-15 flex-column-center-x">
         <div>
             <img height="120" src="/assets/imgs/forum.svg" alt="">
         </div>
-        <div class="text-center">
-            ФОРУМ
-        </div>
-        <div style="padding: 0 25px;">
-            Расскажите свои истории делитесь своим опытом задавайте вопросы
+        <div>
+            <div class="mb-5" style="padding: 0 40px;">
+                ФОРУМ
+            </div>
+            <div style="padding: 0 40px;">
+                Расскажите свои истории делитесь своим опытом задавайте вопросы
+            </div>
         </div>
     </div>
     <div class="scale-1 w-15 flex-column-center-x">
         <div>
             <img height="120" src="/assets/imgs/gallery.svg" alt="">
         </div>
-        <div class="text-center">
-            ГАЛЕРЕЯ
-        </div>
-        <div style="padding: 0 25px;">
-            Работы выполненные нашей командой не тратье время на поиски подберите себе готовые идеи
+        <div>
+            <div class="mb-5" style="padding: 0 40px;">
+                ГАЛЕРЕЯ
+            </div>
+            <div style="padding: 0 40px;">
+                Работы выполненные нашей командой не тратье время на поиски подберите себе готовые идеи
+            </div>
         </div>
     </div>
 </div>
