3339823ee88a3bb1d5cd5e438b4c1b42e99bfb53
Sergey <veselok@gmail.com>
2023-03-15 13:25:23 +0300

    adaptive

--- resources/views/new-design/app.blade.php
+++ resources/views/new-design/app.blade.php
@@ -222,6 +222,11 @@
             justify-content: space-between;
         }
 
+        .flex-around-x {
+            display: flex;
+            justify-content: space-around;
+        }
+
         .flex-wrap-evenly-x {
             display: flex;
             flex-wrap: wrap;
@@ -905,6 +910,51 @@
                 </div>
             </div>
         </footer>
+        <div class="show-adaptive">
+            <div class="flex-column-center" style="position: fixed; right: 0; bottom: 70px;">
+                <div class="mb-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="24" fill="green" class="bi bi-telephone-inbound-fill" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM15.854.146a.5.5 0 0 1 0 .708L11.707 5H14.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.793L15.146.146a.5.5 0 0 1 .708 0z"></path>
+                    </svg>
+                </div>
+                <div class="mb-5">
+                    <a class="mx-10 whatsapp" href="https://api.whatsapp.com/send/?phone={}">
+                        <img class="mx-10" width="26" src="http://victoria.local/icon/whatsapp.svg" alt="">
+                    </a>
+                </div>
+                <div class="mb-5">
+                    <a class="mx-10 telegram" href="https://t.me/{}">
+                        <img class="mx-10" width="24" src="http://victoria.local/icon/telegram.svg" alt="">
+                    </a>
+                </div>
+            </div>            <div class="mob-menu" style="position: fixed; bottom: 0; height: 60px; width: 100vw;">
+                <div class="flex-around-x" style="background-color: var(--main-bg-color); padding: 10px; color: white; height: calc(100% - 20px)">
+                    <div>
+                        <img width="30" src="/assets/imgs/email.svg" alt="email">
+                    </div>
+                    <div>
+                        <img width="30" src="/assets/imgs/email.svg" alt="email">
+                    </div>
+                    <div data-relation-id="mobMenu">
+                        <img width="30" src="/assets/imgs/catalog.svg" alt="catalog">
+                    </div>
+                    <div>
+                        <img width="30" src="/assets/imgs/profile.svg" alt="profile">
+                    </div>
+                    <div>
+                        <img width="30" src="/assets/imgs/basket.svg" alt="basket">
+                    </div>
+                </div>
+            </div>
+            <div id="mobMenu" class="hide" style="z-index:3; position: fixed; top: 0; left: 0; background-color: rgba(0,0,0,0.8); color: white; width: 100vw; height: calc(100vh - 60px);">
+                <div style="overflow: scroll;height: calc(100% - 20px);padding: 10px;">
+                    <div class="border-radius-25 p-10 mb-10 mt-10">Главная</div>
+                    <div class="border-radius-25 p-10 mb-10 mt-10">Быстрое оформление</div>
+                    <div class="border-radius-25 p-10 mb-10 mt-10">Онлайн заказ</div>
+                    <div class="border-radius-25 p-10 mb-10 mt-10">О компании</div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <script>
