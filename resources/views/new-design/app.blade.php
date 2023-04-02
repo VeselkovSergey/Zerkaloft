142422f8d31e07041040853d9548d389cfd8d22f
Sergey <veselok@gmail.com>
2023-04-02 20:03:27 +0300

    new design favicon

--- resources/views/new-design/app.blade.php
+++ resources/views/new-design/app.blade.php
@@ -30,6 +30,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="yandex-verification" content="1bc095e408e6a616" />
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="favicon.svg">
 
     <title>{{ isset($title_page) ? $title_page : env('APP_NAME') }}</title>
 
