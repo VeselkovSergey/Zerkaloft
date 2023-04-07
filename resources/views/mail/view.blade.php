4f6a69c6c765d579d134dc0b150b3813f0548d5b
Sergey <veselok@gmail.com>
2023-04-07 13:38:24 +0300

    new design fix

--- resources/views/mail/view.blade.php
+++ resources/views/mail/view.blade.php
@@ -4,13 +4,8 @@
     </div>
     <div style="text-align: center;">
         <h3>Добро пожаловать</h3>
-        <h3 style="color: #2e3192;">Victoria Рекламное агентство</h3>
         <div>Для входа в личный кабинет используйте:</div>
         <div><span style="color: #2e3192;">Логин:</span> {{$email}}</div>
         <div><span style="color: #2e3192;">Пароль:</span> {{$password}}</div>
-        <div style="text-align: left; width: 100%">
-            <div>С уважением,</div>
-            <div style="color: #2e3192;">Victoria Рекламное агентство</div>
-        </div>
     </div>
 </div>
