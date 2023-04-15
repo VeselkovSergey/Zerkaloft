6bd2dc519acce460f4c93ec889b7b694fc2685ef
Sergey <veselok@gmail.com>
2023-04-15 15:32:06 +0300

    new design fix

--- resources/views/new-design/text.blade.php
+++ resources/views/new-design/text.blade.php
@@ -4,6 +4,13 @@
 
 @section("content")
     <style>
+        pre {
+            white-space: pre-wrap;       /* Since CSS 2.1 */
+            white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+            white-space: -pre-wrap;      /* Opera 4-6 */
+            white-space: -o-pre-wrap;    /* Opera 7 */
+            word-wrap: break-word;       /* Internet Explorer 5.5+ */
+        }
         .wrapper {
             position: relative;
             border-right: unset;
