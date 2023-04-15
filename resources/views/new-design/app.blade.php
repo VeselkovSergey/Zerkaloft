5e7c4fa99ffe70badffbd2bddd460a3e1c4687ac
Sergey <veselok@gmail.com>
2023-04-15 16:13:44 +0300

    new design fix

--- resources/views/new-design/app.blade.php
+++ resources/views/new-design/app.blade.php
@@ -32,7 +32,7 @@
 
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="yandex-verification" content="1bc095e408e6a616" />
+    <meta name="yandex-verification" content="f23e503553472595" />
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="icon" type="image/x-icon" href="/favicon.svg">
 
