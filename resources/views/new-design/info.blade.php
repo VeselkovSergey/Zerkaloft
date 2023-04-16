2b65893db4efb62600a00997a53b97768d6f99d6
Sergey <veselok@gmail.com>
2023-04-16 13:48:12 +0300

    new design fix

--- resources/views/new-design/info.blade.php
+++ resources/views/new-design/info.blade.php
@@ -9,7 +9,7 @@
     }
 </style>
 <div class="info-block p-20 bg-pink" style="color: black;">
-    <a href="{{route("about-page")}}#information" class="block scale-1 w-15-adaptive-40vw flex-column-center-x">
+    <a href="{{route("about-page")}}#interesting" class="block scale-1 w-15-adaptive-40vw flex-column-center-x">
         <div class="img-120-adaptive-70">
             <img src="/assets/imgs/info.2.svg" alt="">
         </div>
