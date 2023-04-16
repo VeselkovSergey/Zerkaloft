9272b463dda3db122e2f8f660f057ab86e89ed28
Sergey <veselok@gmail.com>
2023-04-16 13:07:20 +0300

    new design fix

--- resources/views/new-design/info.blade.php
+++ resources/views/new-design/info.blade.php
@@ -1,10 +1,10 @@
 <style>
-    .px-40-adaptive-0 {
-        padding: 0 40px;
+    .px-20-adaptive-0 {
+        padding: 0 20px;
     }
     @media screen and (max-width: 540px) {
-        .px-40-adaptive-0 {
-            padding: 0 40px;
+        .px-20-adaptive-0 {
+            padding: 0 20px;
         }
     }
 </style>
@@ -14,7 +14,7 @@
             <img src="/assets/imgs/info.2.svg" alt="">
         </div>
         <div class="mt-10">
-            <div class="font-light px-40-adaptive-0">
+            <div class="font-light px-20-adaptive-0">
                 Информация
             </div>
         </div>
@@ -24,7 +24,7 @@
             <img src="/assets/imgs/online-order.2.svg" alt="">
         </div>
         <div class="mt-10">
-            <div class="font-light px-40-adaptive-0">
+            <div class="font-light px-20-adaptive-0">
                 Задайте параметры, не тратьте время на поиски
             </div>
         </div>
@@ -34,7 +34,7 @@
             <img src="/assets/imgs/payments.2.svg" alt="">
         </div>
         <div class="mt-10">
-            <div class="font-light px-40-adaptive-0">
+            <div class="font-light px-20-adaptive-0">
                 Быстро и безопасно оплати свой заказ
             </div>
         </div>
@@ -44,7 +44,7 @@
             <img src="/assets/imgs/delivered.2.svg" alt="">
         </div>
         <div class="mt-10">
-            <div class="font-light px-40-adaptive-0">
+            <div class="font-light px-20-adaptive-0">
                 Надежная быстрая доставка ваших покупок
             </div>
         </div>
@@ -54,7 +54,7 @@
             <img src="/assets/imgs/forum.svg" alt="">
         </div>
         <div class="mt-10">
-            <div class="font-light px-40-adaptive-0">
+            <div class="font-light px-20-adaptive-0">
                 Расскажите свои истории делитесь своим опытом задавайте вопросы
             </div>
         </div>
@@ -64,7 +64,7 @@
             <img src="/assets/imgs/gallery.svg" alt="">
         </div>
         <div class="mt-10">
-            <div class="font-light px-40-adaptive-0">
+            <div class="font-light px-20-adaptive-0">
                 Работы выполненные нашей командой не тратье время на поиски подберите себе готовые идеи
             </div>
         </div>
