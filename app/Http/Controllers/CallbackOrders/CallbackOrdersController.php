29d99176dfcadd910996772e9253891c4858266d
Sergey <veselok@gmail.com>
2023-04-18 13:18:56 +0300

    new design fix

--- app/Http/Controllers/CallbackOrders/CallbackOrdersController.php
+++ app/Http/Controllers/CallbackOrders/CallbackOrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\CallbackOrders;
 
 use App\Http\Controllers\Controller;
 use App\Models\CallbackOrders;
+use App\Services\Telegram\Telegram;
 
 class CallbackOrdersController extends Controller
 {
@@ -20,6 +21,11 @@ class CallbackOrdersController extends Controller
             'name' => $name,
             'phone' => $phone
         ]);
+
+        $message = '<i>Новый запрос.</i><a href="'.route("all-callback-orders").'">к запросам</a>' . PHP_EOL;
+
+        $telegram = new Telegram();
+        $telegram->sendMessage($message, env('TELEGRAM_ORDER_GROUP'));
         return $newCallbackOrder;
     }
 
