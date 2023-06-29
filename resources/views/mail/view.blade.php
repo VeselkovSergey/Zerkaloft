b0a6af2ce522be97a00132cc8f01905268cd2a82
Sergey <veselok@gmail.com>
2023-06-29 20:53:15 +0300

    почта

--- resources/views/mail/view.blade.php
+++ resources/views/mail/view.blade.php
@@ -1,6 +1,7 @@
-<div style="text-align: center; background-image: url('{{asset('img/bg.jpeg')}}'); background-repeat: no-repeat; background-size: contain;">
+<div style="text-align: center; background-image: url('{{asset('img/new-bg.jpeg')}}'); background-repeat: no-repeat; background-size: contain;">
     <div>
-        <img width="200" height="60" src="{{asset('icon/logo_rus.png')}}" alt="">
+{{--        <img width="200" height="60" src="{{asset('icon/logo_rus.png')}}" alt="">--}}
+        <img width="200" height="60" src="{{route('files', \App\Http\Controllers\Administration\SettingsController::GetHeaderLogo()->imageFileId) ?? "/assets/imgs/logo.svg"}}" alt="">
     </div>
     <div style="text-align: center;">
         <h3>Добро пожаловать</h3>
