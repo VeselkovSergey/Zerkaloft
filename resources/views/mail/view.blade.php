1b94ed68dc634aa3772f774e4268a2c8b70d1881
Sergey <veselok@gmail.com>
2023-06-29 21:11:53 +0300

    почта

--- resources/views/mail/view.blade.php
+++ resources/views/mail/view.blade.php
@@ -1,4 +1,4 @@
-<div style="text-align: center; background-image: url('{{asset('img/new-bg.jpeg')}}'); background-repeat: no-repeat; background-size: cover;">
+<div style="text-align: center; background-image: url('{{asset('img/new-bg.jpeg')}}'); background-repeat: no-repeat; background-size: cover; padding: 20px;">
     <div>
         <img width="140" height="75" src="{{asset('icon/new_logo.png')}}" alt="">
     </div>
