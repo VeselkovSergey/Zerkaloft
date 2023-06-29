403ff91887d969e74a4f25ba3ac29e30ca3513e0
Sergey <veselok@gmail.com>
2023-06-29 20:55:49 +0300

    почта

--- resources/views/mail/view.blade.php
+++ resources/views/mail/view.blade.php
@@ -1,12 +1,12 @@
-<div style="text-align: center; background-image: url('{{asset('img/new-bg.jpeg')}}'); background-repeat: no-repeat; background-size: contain;">
+<div style="text-align: center; background-image: url('{{asset('img/new-bg.jpeg')}}'); background-repeat: no-repeat; background-size: cover;">
     <div>
 {{--        <img width="200" height="60" src="{{asset('icon/logo_rus.png')}}" alt="">--}}
         <img width="200" height="60" src="{{route('files', \App\Http\Controllers\Administration\SettingsController::GetHeaderLogo()->imageFileId) ?? "/assets/imgs/logo.svg"}}" alt="">
     </div>
-    <div style="text-align: center;">
+    <div style="text-align: center; color: white;">
         <h3>Добро пожаловать</h3>
         <div>Для входа в личный кабинет используйте:</div>
-        <div><span style="color: #2e3192;">Логин:</span> {{$email}}</div>
-        <div><span style="color: #2e3192;">Пароль:</span> {{$password}}</div>
+        <div>Логин: <b>{{$email}}</b></div>
+        <div>Пароль: <b>{{$password}}</b></div>
     </div>
 </div>
