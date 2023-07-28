2a1e80f47f981eabdc6cfeea848687dd606b7530
Sergey <veselok@gmail.com>
2023-07-28 12:11:18 +0300

    fix

--- resources/views/new-design/index.blade.php
+++ resources/views/new-design/index.blade.php
@@ -68,7 +68,7 @@
                         </div>
                         <div class="product-description z-1 pos-abs-adaptive-static">
                             <div class="flex-column-center p-20" style="height: calc(100% - 40px)">
-                                <div class="border-radius-25 show-adaptive p-10 w-100 mb-a text-center">{{$product->title}}</div>
+                                <div class="border-radius-25 show-adaptive p-10 w-100 mb-a text-center h-100">{{$product->title}}</div>
                                 <div class="hide-adaptive p-10 w-100 mb-a">{{$product->title}}</div>
                                 {{--                        @foreach($product->Properties() as $property)--}}
                                 {{--                            <div class="font-light">{{$property->value}}</div>--}}
