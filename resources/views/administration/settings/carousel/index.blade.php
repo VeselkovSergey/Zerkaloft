6c3750fb18113110b1eeab449990cb2c4e7dd80d
Sergey <veselok@gmail.com>
2023-10-08 12:48:15 +0300

    fix

--- resources/views/administration/settings/carousel/index.blade.php
+++ resources/views/administration/settings/carousel/index.blade.php
@@ -7,7 +7,7 @@
         @foreach($carouselImages as $carouselImage)
             <div class="flex-space-between border p-5 m-5">
                 <img width="200" src="{{route('files', json_decode($carouselImage->value)->fileId[0])}}" alt="">
-                <img width="200" class="mr-a" src="{{route('files', json_decode($carouselImage->value)->fileId[1])}}" alt="">
+                <img width="200" class="mr-a" src="{{route('files', json_decode($carouselImage->value)->fileId[1] ?? 0)}}" alt="">
                 <div class="mr-10" data-product-id="{{$carouselImage->id}}">Очередь: {{json_decode($carouselImage->value)->sequence}}</div>
                 <a href="{{route('edit-carousel-image-page', $carouselImage->id)}}">Редактировать</a>
             </div>
